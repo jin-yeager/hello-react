11fdca914bcde5ae24b117fc6f4b14d00849af28
JIN_DDIT <zanalyst01@gmail.com>
2025-05-28 16:53:42 +0900

    feat: LottoGenerator

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import Gugu from "./Gugu.jsx";
 import Holl from "./Holl.jsx";
+import Lotto from "./Lotto.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Holl />
+    <Lotto/>
   </StrictMode>,
 )
